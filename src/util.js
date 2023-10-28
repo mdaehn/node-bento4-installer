d006c194138ac66a1d63826f72776dcc4efc2124
Desmond Kyeremeh <elDekyfin@gmail.com>
2023-10-28 14:10:15 +0000

    Upgraded to 1.0.8

--- src/util.js
+++ src/util.js
@@ -12,7 +12,6 @@ module.exports = {
   removeFileExtension,
   getExecutableFiles,
   getModuleRootPath,
-  getModuleRootPath,
   isFile,
   isDirectory,
   binPath,
@@ -22,23 +21,21 @@ module.exports = {
   platform
 }
 
-
-
-
 function removeFileExtension(filename) {
   const endIndex = filename.lastIndexOf('.')
   return endIndex === -1 ? filename : filename.slice(0, endIndex)
 }
 
 function getExecutableFiles() {
-  return fs.readdirSync(binPath)
-          .filter(filename => fs.statSync(path.join(binPath, filename)).isFile())
-          .map(filename => {
-            return {
-              name: filename,
-              path: path.join(binPath, filename)
-            }
-          })
+  return fs
+    .readdirSync(binPath)
+    .filter(filename => fs.statSync(path.join(binPath, filename)).isFile())
+    .map(filename => {
+      return {
+        name: filename,
+        path: path.join(binPath, filename)
+      }
+    })
 }
 
 function getModuleRootPath() {
@@ -46,7 +43,7 @@ function getModuleRootPath() {
   let rootPath = path.resolve(__dirname, 'node_modules', 'bento4-installer')
 
   // NPM 3
-  if(isDirectory(rootPath) === false) {
+  if (isDirectory(rootPath) === false) {
     rootPath = path.resolve(__dirname, '..')
   }
 
@@ -54,19 +51,19 @@ function getModuleRootPath() {
 }
 
 function isFile(file) {
-    try {
-        const stats = fs.statSync(file)
-        return stats.isFile()
-    } catch (ignore) {
-        return false
-    }
+  try {
+    const stats = fs.statSync(file)
+    return stats.isFile()
+  } catch (ignore) {
+    return false
+  }
 }
 
 function isDirectory(dirPath) {
   try {
     const stats = fs.statSync(dirPath)
     return stats.isDirectory()
-  } catch(ignore) {
+  } catch (ignore) {
     return false
   }
 }
